8d08639031237b6d26d10b3e0db8154f719d5012
Alexandr Levichev <alex-levichev@mail.ru>
2023-11-04 18:46:17 +0300

    added check for softAssertsExtension

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -28,8 +28,7 @@ public class FirstTest {
     $(".markdown-body").$("ul").$$("li").shouldHave(CollectionCondition.itemWithText("Soft assertions"));
     $(byText("Soft assertions")).click();
     $("#user-content-3-using-junit5-extend-test-class").shouldHave(text("3. Using JUnit5 extend test class:"));
-    $("#user-content-3-using-junit5-extend-test-class").sibling(0)
-                .shouldHave(exactText("@ExtendWith({SoftAssertsExtension.class})" +
+    $("#user-content-3-using-junit5-extend-test-class").sibling(0).shouldHave(exactText("@ExtendWith({SoftAssertsExtension.class})" +
                         "\n" +
                         "class Tests {\n" +
                         "@Test\n" +
