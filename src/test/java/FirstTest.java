68b53e9f00a995686ab5a69a302ac7d6e9349d59
Alexandr Levichev <alex-levichev@mail.ru>
2023-11-04 18:44:07 +0300

    added check for softAssertsExtension

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -3,7 +3,8 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.CollectionCondition.texts;
+
+import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
@@ -14,7 +15,6 @@ public class FirstTest {
     static void beforeAll() {
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
-        Configuration.holdBrowserOpen = true;
     }
 
     @Test
@@ -28,5 +28,20 @@ public class FirstTest {
     $(".markdown-body").$("ul").$$("li").shouldHave(CollectionCondition.itemWithText("Soft assertions"));
     $(byText("Soft assertions")).click();
     $("#user-content-3-using-junit5-extend-test-class").shouldHave(text("3. Using JUnit5 extend test class:"));
+    $("#user-content-3-using-junit5-extend-test-class").sibling(0)
+                .shouldHave(exactText("@ExtendWith({SoftAssertsExtension.class})" +
+                        "\n" +
+                        "class Tests {\n" +
+                        "@Test\n" +
+                        "void test() {\n" +
+                        "Configuration.assertionMode = SOFT;\n" +
+                        "open(\"page.html\");\n" +
+                        "\n" +
+                        "$(\"#first\").should(visible).click();\n" +
+                        "$(\"#second\").should(visible).click();\n" +
+                        "}\n" +
+                        "}"));
+
     }
 }
+
